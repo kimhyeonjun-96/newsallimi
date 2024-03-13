be0d3d98e9bf8a5ca99d660094b56f96c00c9029
김현준 <khjlove8427@naver.com>
2024-03-13 17:43:12 +0900

    뉴스 알리미 프로젝트 레포지토리 생성

---
+++ src/main/java/hello/newsallimi/NewsAllimiApplication.java
@@ -0,0 +1,13 @@
+package hello.newsallimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAllimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAllimiApplication.class, args);
+	}
+
+}
